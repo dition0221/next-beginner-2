e6179c26ff7a292d1a0f4dcea4ec8aa90b5bd091
dition0221 <129196812+dition0221@users.noreply.github.com>
2023-12-28 17:59:25 +0900

    23-12-28 / CSS(1) --Fixed redirect

--- src/pages/movies/[...params].tsx
+++ src/pages/movies/[...params].tsx
@@ -16,20 +16,17 @@ export default function Detail({ movie, ssrError }: IDetailProps) {
   return (
     <>
       <Seo title={movie?.title || "Error"} />
+      <article />
 
       <main>
-        <img
-          src={`https://image.tmdb.org/t/p/w500${movie?.backdrop_path}`}
-          alt="backdrop_image"
-        />
         <h4>
-          {`${movie?.title} (${movie?.release_date.slice(0, 4)})` || "Error"}
+          {`${movie?.title} (${movie?.release_date?.slice(0, 4)})` || "Error"}
         </h4>
         <span>{movie?.tagline}</span>
         <span>{`runtime : ${movie?.runtime} minutes`}</span>
         <p>{movie?.overview}</p>
         <ul>
-          {movie?.genres.map((genre) => (
+          {movie?.genres?.map((genre) => (
             <li key={genre.id}>{genre.name}</li>
           ))}
         </ul>
@@ -43,8 +40,16 @@ export default function Detail({ movie, ssrError }: IDetailProps) {
       </main>
 
       <style jsx>{`
-        img {
-          width: 100%;
+        article {
+          /* background-image: url(); */
+          position: fixed;
+          left: 0;
+          right: 0;
+          top: 0;
+          bottom: 0;
+          max-width: 100%;
+          height: 100vh;
+          z-index: -1;
         }
         span {
           display: block;
@@ -64,13 +69,12 @@ export async function getServerSideProps({
       !Array.isArray(params) ||
       params?.length !== 2
     )
-      throw new Error("URL doesn't exist.");
+      return { redirect: { destination: "/", permanent: true } };
     // Fetch API: 'api/detail/[...params]'
     const movieId = params[1];
     const movie: IMovieDetail = await (
       await fetch(`http://localhost:3000/api/detail/${movieId}`)
     ).json();
-
     return { props: { movie } };
   } catch (error) {
     const ssrError = `Error fetching movie from SSR. ❌${error}`;
