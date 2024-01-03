912726ab96fcc0b359a970e16da3bd50186a4730
dition0221 <qjatn8180@naver.com>
2024-01-04 00:44:44 +0900

    TEST deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.NODE_ENV === "production" ? "/next-beginner-2" : "",
+  // Deploy gh-pages config.
+  // output: "export",
+  basePath:
+    process.env.NODE_ENV === "production"
+      ? "https://dition0221.github.io/next-beginner-2/"
+      : "",
 };
 
 module.exports = nextConfig;
